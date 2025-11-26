499058bac1e848b043899b4742655756b049b796
Apeksha-1-hub <meeraban123@gmail.com>
2025-11-26 11:38:25 +0545

    first commit

---
+++ Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Car car = new Car
+        {
+            Brand = "Toyota",
+            Speed = 120,
+            Seats = 5
+        };
+
+        Motorcycle bike = new Motorcycle
+        {
+            Brand = "Yamaha",
+            Speed = 100,
+            HasCarrier = true
+        };
+
+        // Base class methods – reuse
+        car.Start();
+        car.DisplayInfo();
+        car.Stop();
+
+        Console.WriteLine();
+
+        bike.Start();
+        bike.DisplayInfo();
+        bike.Stop();
+    }
+}
